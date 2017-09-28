c95a7cd4c6ae330ee26b28a74b5b8327889ef73b
Caio Belfort <caiobelfort90@gmail.com>
2017-09-28 14:39:27 -0300

    some work in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup
 
 setup(
+    description='Simple functions to make jupyter more clean for presentations',
     name='jupyter_beautifier',
+    version='0.1.0a1',
     author='Caio Belfort',
     author_email='caiobelfort90@gmail.com',
     license='MIT',
     url='https://github.com/caiobelfort/jupyter_beautifier',
     packages=['jupyter_beautifier'],
-    zip_safe=False
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Jupyter Notebook',
+        'Programming Language :: Python 3.6'
+    ],
+    install_requires=['jupyter']
 )
